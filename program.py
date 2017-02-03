1027dad6aff1631db0b1063f732075d9ddd75ef4
Nicolas Mello <nicolasnick700@gmail.com>
2017-02-03 19:44:45 -0300

    Python file modified

--- program.py
+++ program.py
@@ -1,4 +1,16 @@
 #! usr/bin/python
 
-#imprime a menssagem na tela
-print("Ola \nmundo")
+msg = "structure"
+n1 = 90
+n2 = 48
+numberX = 100000
+t = n1 ** 2 / n2
+
+while(t != numberX):
+	print(t)
+	print("-----")
+	t += 1
+
+print("Repetition ", msg)
+print(t)
+

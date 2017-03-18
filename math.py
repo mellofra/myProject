a3f2312fa1c468fb79a08bac670defd9e22548b9
mrsmello <nicolasnick700@gmail.com>
2017-03-18 20:53:09 -0300

    Python file changed(2)

--- math.py
+++ math.py
@@ -8,24 +8,37 @@ name = str(input("Login: "))
 password = str(input("Password: "))
 
 #Exibe o nome e senha
-print("Bem vindo %s" + " " + name)
-print("Senha: %s" + " " + password)
+print(str("Bem vindo "), str(name))
 
-#Solicita o numero para calcular a raiz
+resposta = ""
+while(resposta == ""):
+    resposta = str(input("Mostrar senha?: S ou N"))
+    if(resposta == "S"):
+        print(str("Senha: "), str(password))
+    elif(resposta == "N"):
+        print(str("Senha: ", str("--------")))
+
+#Solicita o numero para tirar a raiz
 n = 0
-while(n == 0):
-    n = int(input("Digite o numero para tirar a raiz :"))
+if(name != "" and password != ""):
+    while(n == 0):
+        n = int(input("Digite o numero para tirar a raiz: "))
 
-#Calcula e mostra a raiz quadrada do numero especificado pelo usuario  
+#Calcula e mostra a raiz quadrada do numero digitado    
 def raiz():
     r = math.sqrt(n)
     if(r % 2 == 0):
-        print(int(r))
+        print(str("Numero: "), int(n))
+        print(str("Raiz:"), float(r))
         print(type(r))
     else:
-        print(float(r))
-        print(type(r))      
+        print(str("Numero: "), int(n))
+        print(str("Raiz"), float(r))
+        print(type(r))
         
-raiz()
+if(name != "" and password != ""):
+    raiz()
+elif(name == "" and password == ""):
+    print("Internal error")
 
 print("fim")

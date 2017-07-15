1396012e4d900886612684f28dcc3afcd15d5bf8
Nicolas Mello <mrsmello@debian>
2017-07-15 18:27:40 -0300

    Hello, World

---
+++ Codes/Haskell_Codes/main.hs
@@ -0,0 +1,6 @@
+import Data.List
+import System.IO 
+
+lista = 2 : 4 : 6 :[]
+
+reLista = reverse lista 
--- python/program.py
+++
@@ -1,9 +0,0 @@
-import sys
-import request
-
-# pega o link passado e mostra o condigo-fonte
-a = sys.argv
-
-l = request.get(a[1])
-
-print(l.text)
